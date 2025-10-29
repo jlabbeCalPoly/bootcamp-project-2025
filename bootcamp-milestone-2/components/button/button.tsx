f11184cf88d152c8ca9ea1eeaf9852a280290884
Julian Labbe <jlabbe@calpoly.edu>
2025-10-28 20:21:30 -0700

    Updates to the logo component to include a profile picture

--- bootcamp-milestone-2/components/button/button.tsx
+++ bootcamp-milestone-2/components/button/button.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "next/link";
 import Style from "./button.module.css";
 
-export default function Button({slug}: {slug : string}) {
+export default function Button({text, slug}: {text : string, slug : string}) {
     return (
         <Link href={slug}>
             <div className={Style.buttonContainer}>
-                <h2>Learn more</h2>
+                <h2>{text}</h2>
                 <img src={"/arrowIcon.svg"}/>
             </div>
         </Link>
--- bootcamp-milestone-2/components/logo/logo.tsx
+++ bootcamp-milestone-2/components/logo/logo.tsx
@@ -4,8 +4,11 @@ import Style from "./logo.module.css";
 
 export default function Logo() {
     return (
-        <h1 className={Style.logo}>
-            <Link href="/">Julian Labbe</Link>
-        </h1>
+        <Link className={Style.logo} href="/">
+            <img src="/jlabbe.jpg"/>
+            <h1>
+                Julian Labbe
+            </h1>
+        </Link>
     );
 }
