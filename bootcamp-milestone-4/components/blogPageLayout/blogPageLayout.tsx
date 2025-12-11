4aaac9c67dbd71e29a21efb3bda8b647c225d74e
Julian Labbe <jlabbe@calpoly.edu>
2025-12-10 19:50:22 -0800

    Added background to blog page

--- bootcamp-milestone-4/components/blogPageLayout/blogPageLayout.tsx
+++ bootcamp-milestone-4/components/blogPageLayout/blogPageLayout.tsx
@@ -1,12 +1,30 @@
+"use client"
 import React from "react";
+import ColorBends from "../backgrounds/colorBends/colorBends";
 import CommentPageLayout from "../iCommentPageLayout/iCommentPageLayout";
 import { BlogType } from "@/src/database/blogSchema";
 import Style from "./blogPageLayout.module.css";
 
 export default function BlogPageLayout({ blogData }: { blogData: BlogType }) {
   return (
-    <div className={Style.blogPageLayoutContainer}>
+    <>
+    <div className={Style.blogPageBackgroundContainer}>
+        <ColorBends
+          colors={["#fffa5cff", "#ff985cff"]}
+          rotation={45}
+          speed={0.2}
+          scale={1}
+          frequency={1}
+          warpStrength={1}
+          mouseInfluence={0}
+          parallax={0.5}
+          noise={0.1}
+          transparent
+        />
+    </div>
+    <div className={Style.blogPageContentContainer}>
       <CommentPageLayout data={blogData.comments} slug={blogData.slug} />
     </div>
+    </>
   );
 }
